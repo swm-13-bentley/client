e3e23fd966e0b8781ea7b97d09317192caf08011
zeroStone ⠀ <zerostone@zeroStoneui-MacBookPro.local>
2022-07-12 18:01:08 +0900

    feat: inject material ui

--- manna-time/pages/_app.tsx
+++ manna-time/pages/_app.tsx
@@ -3,14 +3,25 @@ import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout/Layout'
+import { ThemeProvider } from '@mui/material/styles'
+import { CacheProvider } from '@emotion/react'
+import createEmotionCache from '../src/createEmotionCache'
+import theme from '../src/theme'
+
+const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <CacheProvider value={clientSideEmotionCache}>
+      <ChakraProvider>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </ChakraProvider>
+    </CacheProvider>
   )
 }
 
