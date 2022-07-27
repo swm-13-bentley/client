4f1d95cf39da38fec75bee008aa356fd0b51363a
zeroStone ⠀ <zerostone@zeroStoneui-MacBookPro.local>
2022-07-27 22:52:42 +0900

    feat: mixpanel tracking added

--- manna-time/pages/_app.tsx
+++ manna-time/pages/_app.tsx
@@ -8,9 +8,15 @@ import createEmotionCache from '../src/createEmotionCache'
 import theme from '../src/theme'
 import MainLayout from '../components/Layout/MainLayout/MainLayout'
 
+import mixpanel from 'mixpanel-browser'
+
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  mixpanel.init('de0053b0ed79028b519487b67141eb58', { debug: true })
+  mixpanel.track('Sign up')
+  console.log("hey")
 
   // return (<Component />)
   return (
