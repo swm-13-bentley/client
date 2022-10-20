d20e103286003fd76908df4bc2c928c67db35706
zeroStone ⠀ <zerostone@zeroStoneui-MacBookPro.local>
2022-10-21 00:35:22 +0900

    fix: iframe height tracking

--- manna-time/pages/[locale]/index.tsx
+++ manna-time/pages/[locale]/index.tsx
@@ -1,9 +1,17 @@
 import { NextPage} from "next"
+import { useEffect, useState } from "react";
 
 // const Test: NextPage = function ({ data }:InferGetStaticPropsType<typeof getStaticProps>) {
 const Home: NextPage = function () {
+    const [height, setHeight] = useState(0)
+    useEffect(() => {
+        setHeight(window.innerHeight)
+        window.addEventListener('resize', ()=> {
+            setHeight(window.innerHeight)
+        })
+     }, [])
     return (
-        <iframe src="/landingPage/index.html" style={{ display: 'block', width: '100vw', height: "calc(100vh - 59px) !important" }}></iframe>
+        <iframe src="/landingPage/index.html" style={{ display: 'block', width: '100vw', height: `calc(${height}px - 59px)` }}></iframe>
     )
 }
 
