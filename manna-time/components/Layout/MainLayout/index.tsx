90d4653592845d4283b21cc85fbe29d42f79c8fb
zeroStone ⠀ <zerostone@zeroStoneui-MacBookPro.local>
2022-11-15 14:10:30 +0900

    fix: android to lowercase

--- manna-time/components/Layout/MainLayout/index.tsx
+++ manna-time/components/Layout/MainLayout/index.tsx
@@ -47,7 +47,7 @@ function Layout({ children }: ScriptProps, { userAgent }: AppProps & { userAgent
             // console.log(decodedToken)
         }
 
-        if(navigator.userAgent.toLowerCase().indexOf('Android') != -1){
+        if (navigator.userAgent.toLowerCase().indexOf('android') != -1) {
             if (navigator.userAgent.match(/inapp|NAVER|KAKAOTALK|Snapchat|Line|WirtschaftsWoche|Thunderbird|Instagram|everytimeApp|WhatsApp|Electron|wadiz|AliApp|zumapp|iPhone(.*)Whale|Android(.*)Whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|SamsungBrowser\/[^1]/i)
                 && window.flutter_inappwebview == undefined) {
                 document.body.innerHTML = '';
